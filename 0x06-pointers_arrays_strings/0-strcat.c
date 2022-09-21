85e4151e1113e86e660888d5962fd958dadd310e
usaamah <isholausaamahgmail.com>
2022-09-21 21:15:05 +0000

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcat -. is a function
+ * @dest: first parameter
+ * @src: second parameter
+ * return out put
+ */
+ char *_strcat(char *dest, char *src);
+{
+    int i;
+    int j;
+
+    i = 0;
+	j = 0
+
+	while (dest[i] != '\0')
+       	{
+		i++;
+	}
+
+	while (ssrc[j] != '\0')
+       	{
+		j++;
+		i++;
+	}
+	des[j] = '\0';
+
+	return (dest);
+}
